6a802b93db61f3b1d59ab9eedd0931d143dee928
lmjcbs <hello@liamjacobs.dev>
2020-01-12 20:12:06 +0000

    add set_user user_params helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
 
+  private 
+
+  def set_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end

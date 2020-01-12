ec037ac27d340cb0badea7b42cb97557889c6f66
lmjcbs <hello@liamjacobs.dev>
2020-01-12 20:28:47 +0000

    add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   get '/singup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create'
-  
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 end

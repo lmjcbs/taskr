3f96107d22bdea676bb693899932511b7ae4e9ce
lmjcbs <hello@liamjacobs.dev>
2020-01-12 20:16:24 +0000

    add users#create action for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path
+    else
+      redirect_to signup_path
+    end
+  end
+
   private 
 
   def set_user

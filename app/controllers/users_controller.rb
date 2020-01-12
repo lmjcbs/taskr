f8c785be48fad93e19cf2c3e8d562423c31af410
lmjcbs <hello@liamjacobs.dev>
2020-01-12 20:46:08 +0000

    fixed set_user variable scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,14 @@ class UsersController < ApplicationController
   end
 
   def home
-    set_user
+    @user = set_user
     render 'index'
   end
 
   private 
 
   def set_user
-    @user = User.find_by(id: session[:user_id])
+    User.find_by(id: session[:user_id])
   end
 
   def user_params

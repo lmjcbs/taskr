5a314e50841089587ac7bfc09f8a7e1f99ae47f1
lmjcbs <hello@liamjacobs.dev>
2020-01-13 22:47:33 +0000

    removed category column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_202455) do
+ActiveRecord::Schema.define(version: 2020_01_13_224627) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_01_13_202455) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
-    t.string "category"
     t.string "project_manager"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

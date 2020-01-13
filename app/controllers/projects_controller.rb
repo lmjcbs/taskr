dfaac93f6da726bbb1805dd61e47dffc460b9f1a
lmjcbs <hello@liamjacobs.dev>
2020-01-13 20:59:28 +0000

    add projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,53 @@
+class ProjectsController < ApplicationController
+  before_action :logged_in?
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = "Project successfully created"
+      redirect_to project_path(@project)
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def show
+    @project = set_project
+  end
+
+  def edit
+    @project = set_project
+  end
+
+  def update
+    @project = set_project
+    if @project.update(project_params)
+      flash[:success] = "Project successfully updated"
+      redirect_to project_path(@project)
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = set_project
+    @project.destroy
+    redirect_to home_path
+  end
+  
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :category, :project_manager)
+  end
+  
+  def set_project
+    Project.find_by(id: params[:id])
+  end
+end

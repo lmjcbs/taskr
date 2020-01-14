202b6ffd5053d562378f9cc2b4326ae0a11c68ab
lmjcbs <hello@liamjacobs.dev>
2020-01-14 12:52:34 +0000

    add is_project_manager validations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,10 @@
 class ProjectsController < ApplicationController
   before_action :logged_in?
 
+  def index
+    @projects = current_user.projects
+  end
+  
   def new
     @project = Project.new
   end
@@ -9,7 +13,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
     if @project.save
       flash[:success] = "Project successfully created"
-      redirect_to project_path(@project)
+      redirect_to @project
     else
       flash[:error] = "Something went wrong"
       render 'new'
@@ -18,17 +22,29 @@ class ProjectsController < ApplicationController
 
   def show
     @project = set_project
+    if current_user.is_project_member?(@project)
+      render 'show'
+    else
+      flash[:error] = "You do not have the permissions to view that"
+      redirect_to home_path
+    end
   end
 
   def edit
     @project = set_project
+    if is_project_manager?(@project)
+      render 'edit'
+    else
+      flash[:error] = "You do not have the permissions to do that"
+      redirect_to @project
+    end
   end
 
   def update
     @project = set_project
-    if @project.update_attributes(project_params)
+    if is_project_manager?(@project) && @project.update_attributes(project_params)
       flash[:success] = "Project successfully updated"
-      redirect_to project_path(@project)
+      redirect_to @project
     else
       flash[:error] = "Something went wrong"
       render 'edit'
@@ -37,12 +53,12 @@ class ProjectsController < ApplicationController
 
   def destroy
     @project = set_project
-    if @project.destroy
-      flash[:success] = 'Object was successfully deleted.'
+    if is_project_manager?(@project) && @project.destroy
+      flash[:success] = "Project was successfully deleted."
       redirect_to home_path
     else
-      flash[:error] = 'Something went wrong'
-      redirect_to home_path
+      flash[:error] = "Something went wrong"
+      redirect_to @project
     end
   end
   

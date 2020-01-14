bc42e59d0366a24c8b92279528cc1bc5e15858e8
lmjcbs <hello@liamjacobs.dev>
2020-01-14 18:41:56 +0000

    moved set_project_manager to helper

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,15 +1,7 @@
 class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_many :categories, through: :tasks
   has_many :comments, through: :tasks
-
-  before_save :set_project_manager
-
-  private
-
-  def set_project_manager
-    self.project_manager = current_user
-  end
 end

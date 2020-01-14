603fa64570515419c3f97178d28a1ad1071dced8
lmjcbs <hello@liamjacobs.dev>
2020-01-14 15:40:13 +0000

    add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :user_projects
+  has_many :projects, through: :user_projects
+  has_many :tasks, through: :projects
+  has_many :comments, through: :tasks
+
   has_secure_password
 
   def is_project_member?(project)

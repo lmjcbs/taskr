a4e028f0fca6dd5bb3a69ea90c7a529b248427f6
lmjcbs <hello@liamjacobs.dev>
2020-01-14 12:51:06 +0000

    add current user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    @user ||= User.find_by(id: session[:user])
+  end
 
   def logged_in?
-    redirect_to login_path unless session[:user_id]
+    redirect_to login_path if current_user.nil?
   end
 end

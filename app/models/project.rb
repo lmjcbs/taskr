214fe3a6d3ae64dd95b5bcc28bdb2515daf903c1
lmjcbs <hello@liamjacobs.dev>
2020-01-15 14:21:35 +0000

    include add_project_manager method

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,11 @@ class Project < ApplicationRecord
   has_many :categories, through: :tasks
   has_many :comments, through: :tasks
 
+  def add_project_manager(user)
+    self.project_manager_id = user.id
+    user.projects << self
+  end
+
   def project_manager
     User.find(self.project_manager_id)
   end

923ec54b52631023839f2540bd4db199587e3221
lmjcbs <hello@liamjacobs.dev>
2020-01-15 14:06:04 +0000

    refactored nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#home', as: 'home'
 
+  resources :projects do
+    resources :tasks, except: :index
+  end
+
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
-
-  resources :projects, only: %i[new create show edit update destroy] 
-
-  resources :tasks, only: %i[new create show edit update destroy]
-
-  resources :comments, only: %i[new create show edit update destroy]
 end

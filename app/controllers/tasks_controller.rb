15e1a762b4d5f002c704b5774b41801a67241819
lmjcbs <hello@liamjacobs.dev>
2020-01-15 14:03:20 +0000

    fixed nested redirect_to paths

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,7 @@ class TasksController < ApplicationController
   before_action :set_user
   before_action :set_project
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :created_task?, only: [:edit, :update, :destroy]
-
-  def index
-    @tasks = @project.tasks
-  end
+  before_action :user_created_task?, only: [:edit, :update, :destroy]
 
   def new
     @task = @project.tasks.build
@@ -14,9 +10,10 @@ class TasksController < ApplicationController
 
   def create
     @task = @project.tasks.build(task_params)
+    @task.user = @user
     if @task.save
       flash[:success] =  "Task successfully created"
-      redirect_to project_task_path(@task)
+      redirect_to project_task_path(@project, @task)
     else
       flash[:error] = "Something went wrong"
       render 'new'
@@ -30,9 +27,9 @@ class TasksController < ApplicationController
   end
 
   def update
-    if @task.update_attribute(task_params)
+    if @task.update(task_params)
       flash[:success] = "Task successfully updated"
-      redirect_to project_task_path(@project)
+      redirect_to project_task_path(@project, @task)
     else
       flash[:error] = "Something went wrong"
       render 'edit'
@@ -42,17 +39,17 @@ class TasksController < ApplicationController
   def destroy
     if @task.destroy
       flash[:success] = "Task was successfully deleted."
-      redirect_to projects_path
+      redirect_to @project
     else
       flash[:error] = "Something went wrong"
-      redirect_to project_tasks_path(@project)
+      redirect_to project_task_path(@project, @task)
     end
   end
   
   private
 
   def task_params
-    params.require(:task).permit(:title, :description, :due_date, :assigned_to)
+    params.require(:task).permit(:title, :description, :due_date, :assigned_to, :category_id)
   end
   
   def set_project
@@ -63,8 +60,8 @@ class TasksController < ApplicationController
     @task = @project.tasks.find(params[:id])
   end
   
-  def created_task?
-    unless @task.user_id == @user.id
+  def user_created_task?
+    unless @task.user == @user
       flash[:error] = "You do not have the permissions to do that"
       redirect_to @project
     end

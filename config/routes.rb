933336cedad72ca12a3bd14c6d3390b271e1bc09
lmjcbs <hello@liamjacobs.dev>
2020-01-15 23:50:54 +0000

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#home', as: 'home'
+  root to: 'home#index', as: 'home'
 
   resources :projects do
     resources :tasks do
       resources :comments, only: [:create, :destroy]
     end
   end
+
+  # resources :comments, only: [:destroy]
 
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create'

f9aae11d73eac01ab2398d161639a1553325f216
lmjcbs <hello@liamjacobs.dev>
2020-01-17 15:39:13 +0000

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index', as: 'home'
+  root to: 'home#index'
+
+  devise_for :users
 
   resources :projects do
     resources :tasks do
       resources :comments, only: [:create, :destroy]
     end
   end
-
-  # resources :comments, only: [:destroy]
-
-  get '/signup', to: 'users#new', as: 'signup'
-  post '/signup', to: 'users#create'
-
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
 end

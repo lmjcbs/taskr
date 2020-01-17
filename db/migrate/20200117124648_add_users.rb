b5745fa5c97f3a69ab7bc27b0981c790b4d3ecdb
lmjcbs <hello@liamjacobs.dev>
2020-01-17 15:39:51 +0000

    removed password_digest column from users table

---
+++ db/migrate/20200117124648_add_users.rb
@@ -0,0 +1,9 @@
+class AddUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_233308) do
+ActiveRecord::Schema.define(version: 2020_01_17_124811) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_01_15_233308) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false

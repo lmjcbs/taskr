5eb1ce9dfedee16ea92d57a66493e4246b35c4bb
lmjcbs <hello@liamjacobs.dev>
2020-01-17 23:04:21 +0000

    create user record using omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
+
   has_many :user_projects
   has_many :projects, through: :user_projects
   has_many :tasks, through: :projects
   has_many :comments, through: :tasks
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.username = auth.info.nickname
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end

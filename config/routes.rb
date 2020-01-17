7c3d41c3159142cb6b03cc619ad266c46b5f9e4e
lmjcbs <hello@liamjacobs.dev>
2020-01-17 22:58:53 +0000

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
 
   resources :projects do
     resources :tasks do

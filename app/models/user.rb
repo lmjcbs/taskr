d5907e7ad6ae8af56d8ad642c31d1944d3e87bba
lmjcbs <hello@liamjacobs.dev>
2020-01-22 21:25:17 +0000

    renamed user_projects to projec_members

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable
-
-  has_many :user_projects
-  has_many :projects, through: :user_projects
+  :recoverable, :rememberable, :validatable,
+  :omniauthable
+  has_many :project_members
+  has_many :projects, through: :project_members
   has_many :tasks, through: :projects
   has_many :comments, through: :tasks
+  validates :username, uniqueness: :true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|

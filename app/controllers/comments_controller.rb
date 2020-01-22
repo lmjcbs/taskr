067b3fa82556c7bdc728b53168e21053c5ea3cc0
lmjcbs <hello@liamjacobs.dev>
2020-01-22 21:18:57 +0000

    refactor helper methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,22 @@
 class CommentsController < ApplicationController
-  before_action :set_user
   before_action :set_project
   before_action :set_task
-  before_action :set_comment, only: :destroy
 
   def create
     @comment = @task.comments.build(comment_params)
-    @comment.user = @user
+    @comment.user = current_user
     if @comment.save
       flash[:notice] = "Comment successfully added"
       redirect_to project_task_path(@project, @task)
     else
-      flash[:alert] = "Error creating comment: #{@comment.errors}"
+      flash[:alert] = "Error creating comment"
       redirect_to project_task_path(@project, @task)
     end
   end
 
   def destroy
-    if @comment.destroy
+    comment = Comment.find(params[:id])
+    if comment.destroy
       flash[:notice] = "Comment was successfully deleted."
       redirect_to project_task_path(@project, @task)
     else
@@ -27,11 +26,7 @@ class CommentsController < ApplicationController
   end
 
   private
-
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
+  
   def set_project
     @project = Project.find(params[:project_id])
   end
@@ -41,6 +36,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:content, :user_id, :task_id)
+    params.require(:comment).permit(:content)
   end
 end

047e0243a5ca62dc8faa1627f13246f662116583
lmjcbs <hello@liamjacobs.dev>
2020-01-22 21:22:39 +0000

    refactor project helpers

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,24 @@
 class Project < ApplicationRecord
-  has_many :user_projects
-  has_many :users, through: :user_projects
+  has_many :project_members
+  has_many :users, through: :project_members
   has_many :tasks, dependent: :destroy
-  has_many :categories, through: :tasks
   has_many :comments, through: :tasks
 
-  def add_project_manager(user)
-    self.project_manager_id = user.id
-    user.projects << self
+  validates :title, presence: :true, uniqueness: true
+
+  accepts_nested_attributes_for :tasks
+
+  def add_project_member(user)
+    self.users << user
+  end
+
+  def define_project_manager(user)
+    self.users << user
+    self.project_members[0].role = "Project Manager"
+    self.project_members[0].is_manager = true
   end
 
   def project_manager
-    User.find(self.project_manager_id)
+    User.find(self.project_members[0].user_id)
   end
 end

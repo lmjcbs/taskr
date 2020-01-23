9403e28442cd29dc34c8223b68f7012d85755fd0
lmjcbs <hello@liamjacobs.dev>
2020-01-23 22:48:38 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cat1 = Category.create(name: 'Operations')
+cat2 = Category.create(name: 'Business Development')
+cat3 = Category.create(name: 'Accounting')
+cat4 = Category.create(name: 'Logistics')
+cat5 = Category.create(name: 'Human Resources')
+cat6 = Category.create(name: 'Product Development')
+cat7 = Category.create(name: 'Information Technology')
+cat8 = Category.create(name: 'Customer Service')
+cat9 = Category.create(name: 'Marketing and Advertising')
+cat10 = Category.create(name: 'Legal')

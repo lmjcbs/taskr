a4e207e235cc7244455433c908a359ec17e0bda7
lmjcbs <hello@liamjacobs.dev>
2020-01-28 16:44:11 +0000

    fix validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,6 @@ class Task < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy
 
-  validates :title, :due_date, presence :true
+  validates :title, :due_date, presence: :true
   validates :description, length: { minimum: 15 }
 end
